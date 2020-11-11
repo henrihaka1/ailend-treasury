416914d5590c35b2172846c2ca953e35b551238a
Henri Haka <henri.haka@fti.edu.al>
2020-11-11 23:50:42 +0100

    Angular setup

---
+++ AILendTreasury.Api/Controllers/PositionController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AILendTreasury.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PositionController : ControllerBase
+    {
+        [HttpGet("test")]
+        [ProducesResponseType(200)]
+        public void TestMethod()
+        {
+            Console.WriteLine("This Works");
+        }
+    }
+}
